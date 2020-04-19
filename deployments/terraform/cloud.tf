83d301da3bea03210a64f69f10893272335f2c47
tunji57 <tunji57@gmail.com>
2020-04-18 22:22:03 -0500

    fixed spacing issue

--- deployments/terraform/cloud.tf
+++ deployments/terraform/cloud.tf
@@ -1,6 +1,6 @@
 module "nextcloud_deploy" {
- source  = "fuchicorp/chart/helm"
-  version = "0.0.3"
+ source                  = "fuchicorp/chart/helm"
+  version                = "0.0.3"
   deployment_name        = "nextcloud-deployment"
   deployment_environment = "dev"
   deployment_endpoint    =  "${lookup(var.deployment_endpoint, "${var.deployment_environment}")}"
